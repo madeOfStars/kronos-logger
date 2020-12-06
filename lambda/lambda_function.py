32954a6db44cb2c656b0163ecdcbf45214c1f295
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-12-06 17:03:43 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -28,7 +28,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         speak_output = "Welcome, to your first Intent. So far I support only the start of day Intent"
-        ask_output = "What would you like to do?"
+        ask_output = "What would you like Kronos to do?"
 
         return (
             handler_input.response_builder
