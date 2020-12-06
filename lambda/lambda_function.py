7dbe6dfe265ebb77eb3810891844ffc4198495f4
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-12-06 18:39:27 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -81,7 +81,7 @@ class LogStartingDayIntentHandler(AbstractRequestHandler):
         current_time = datetime.strptime(formatted_time_string, "%H:%M").time()
         formatted_time = current_time.strftime("%H:%M")
         
-        speak_output = "You started your day at {starting_time}. Have a great day!".format(starting_time = current_string_time)
+        speak_output = "You started your day at {starting_time}. Have a great day!".format(starting_time = formatted_time)
         
         return (
             handler_input.response_builder
