48e395892f549e3d54c8c36eb07a129f4ff07743
Ertjon Mecka <ertjon.mecka@h-d-gmbh.de>
2020-12-30 01:43:59 +0100

    started working with the start break time

--- lambda/session_utils.py
+++ lambda/session_utils.py
@@ -1,26 +1,14 @@
 def save_start_of_day(handler_input, time):
-    attr = handler_input.attributes_manager.persistent_attributes
-    attr['start_of_day'] = time;
-
-    handler_input.attributes_manager.session_attributes = attr
-
-    handler_input.attributes_manager.save_persistent_attributes()
+    save_attribute('start_of_day', handler_input, time)
 
 
 def save_start_of_break(handler_input, time):
-    attr = handler_input.attributes_manager.persistent_attributes
-    
-    attr['start_of_break'] = time;
-
-    handler_input.attributes_manager.session_attributes = attr
-
-    handler_input.attributes_manager.save_persistent_attributes()
+    save_attribute('start_of_break', handler_input, time)
 
 
 def save_attribute(attribute_name, handler_input, time):
     attr = handler_input.attributes_manager.persistent_attributes
-    if not attr:
-        attr['start_of_day'] = time;
+    attr[attribute_name] = time;
 
     handler_input.attributes_manager.session_attributes = attr
 
