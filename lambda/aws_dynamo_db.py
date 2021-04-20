b8fe8fd56f388ffc04104374177ed3b688b7769a
Ertjon Mecka <ertjon.mecka@h-d-gmbh.de>
2021-04-20 22:07:27 +0200

    test to push to aws dynamo db

--- lambda/aws_dynamo_db.py
+++ lambda/aws_dynamo_db.py
@@ -5,7 +5,7 @@ from boto3.session import ResourceNotExistsError
 class AwsDynamo:
     def __init__(self):
         self.sts_client = boto3.client('sts')
-        self.assumed_role_object=sts_client.assume_role(RoleArn="arn:aws:iam::074295620416:role/AlexaHostedSkillLambdaRole", RoleSessionName="AssumeRoleSession1")
+        self.assumed_role_object=self.sts_client.assume_role(RoleArn="arn:aws:iam::074295620416:role/AlexaHostedSkillLambdaRole", RoleSessionName="AssumeRoleSession1")
         self.credentials=assumed_role_object['Credentials']
 
         # 2. Make a new DynamoDB instance with the assumed role credentials
